64dc96230ad4007b51df1b2171f5000253b6bb36
gitdy <gitdy123>
2016-11-18 13:32:00 +0800

    提交

--- app/src/main/java/com/testlab/dy/dylab/MainActivity.java
+++ app/src/main/java/com/testlab/dy/dylab/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 //public class MainActivity extends Activity {
     ListView mLv;
-    ListView lv2
+    ListView lv2;
     MainListAdapter mAdapter;
     List<String> mList;
 
